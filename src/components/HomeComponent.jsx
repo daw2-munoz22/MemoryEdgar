88c8622996d782ef97e9f34e107201d2a936f4d5
daw2-munoz22 <91275885+daw2-munoz22@users.noreply.github.com>
2024-05-12 22:02:58 +0200

    Historia 9 casi acabada y proyecto al 95%

--- src/components/HomeComponent.jsx
+++ src/components/HomeComponent.jsx
@@ -31,20 +31,13 @@ export default function HomeComponent() {
                                             </div>
                                             <div className="h-64 w-44 overflow-hidden rounded-lg">
                                                 <img
-                                                    src="https://tailwindui.com/img/ecommerce-images/home-page-03-hero-image-tile-02.jpg"
+                                                    src="/ironman.jpeg"
                                                     alt=""
                                                     className="h-full w-full object-cover object-center"
                                                 />
                                             </div>
                                         </div>
                                         <div className="grid flex-shrink-0 grid-cols-1 gap-y-6 lg:gap-y-8">
-                                            <div className="h-64 w-44 overflow-hidden rounded-lg">
-                                                <img
-                                                    src="https://tailwindui.com/img/ecommerce-images/home-page-03-hero-image-tile-03.jpg"
-                                                    alt=""
-                                                    className="h-full w-full object-cover object-center"
-                                                />
-                                            </div>
                                             <div className="h-64 w-44 overflow-hidden rounded-lg">
                                                 <img
                                                     src="/squirtle.jpg"
@@ -54,7 +47,7 @@ export default function HomeComponent() {
                                             </div>
                                             <div className="h-64 w-44 overflow-hidden rounded-lg">
                                                 <img
-                                                    src="https://tailwindui.com/img/ecommerce-images/home-page-03-hero-image-tile-05.jpg"
+                                                    src="/piplup.jpeg"
                                                     alt=""
                                                     className="h-full w-full object-cover object-center"
                                                 />
@@ -63,14 +56,14 @@ export default function HomeComponent() {
                                         <div className="grid flex-shrink-0 grid-cols-1 gap-y-6 lg:gap-y-8">
                                             <div className="h-64 w-44 overflow-hidden rounded-lg">
                                                 <img
-                                                    src="https://tailwindui.com/img/ecommerce-images/home-page-03-hero-image-tile-06.jpg"
+                                                    src="/hulk.jpeg"
                                                     alt=""
                                                     className="h-full w-full object-cover object-center"
                                                 />
                                             </div>
                                             <div className="h-64 w-44 overflow-hidden rounded-lg">
                                                 <img
-                                                    src="https://tailwindui.com/img/ecommerce-images/home-page-03-hero-image-tile-07.jpg"
+                                                    src="/bulbasaur.jpeg"
                                                     alt=""
                                                     className="h-full w-full object-cover object-center"
                                                 />

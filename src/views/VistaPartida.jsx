6bd59ad2af60fc4fb02cfff8cc7ca36c28e3a895
daw2-munoz22 <91275885+daw2-munoz22@users.noreply.github.com>
2024-05-12 21:50:57 +0200

    Historia 9 casi acabada y proyecto al 95%

--- src/views/VistaPartida.jsx
+++ src/views/VistaPartida.jsx
@@ -1,22 +1,20 @@
-import { useEffect, useState } from "react";
+import {useEffect, useState} from "react";
 import { supabase } from "../database/supabase.js";
-import { useClicks } from "../context/ClicksContext.jsx";
+import {useUser} from "../context/UserContext.jsx";
+import {useClicks} from "../context/ClicksContext.jsx";
 
 export const VistaPartida = () => {
 
     const [partidas, setPartidas] = useState([]);
+    const [profiles, setProfile] = useState([]);
+    const {totalClicks} = useClicks();
+    const {user} = useUser();
 
     useEffect(() => {
-        async function fetchData() {
-            const profiles = await loadProfiles();
-            const partidas = await loadPartidas(profiles);
-            setPartidas(partidas);
-        }
-
-        fetchData();
+        loadPartidas(); // Carga las partidas cuando el componente se monta
     }, []);
 
-    async function loadPartidas(profiles) {
+    async function loadPartidas() {
         try {
             let { data: partidas, error } = await supabase
                 .from('partidas')
@@ -25,30 +23,43 @@ export const VistaPartida = () => {
 
             if (error) throw error;
 
-            // Mapear los IDs de usuario a nombres de usuario en las partidas
-            partidas.forEach(partida => {
-                partida.usuari = profiles.find(profile => profile.id === partida.usuari)?.name || "Usuario Desconocido";
-            });
-
-            return partidas;
+            setPartidas(partidas);
         } catch (error) {
             console.error('Error loading partidas:', error);
-            return [];
         }
     }
 
-    async function loadProfiles() {
+    //FUNCION PESADILLA
+    async function loadProfiles(id) {
         try {
-            let { data: profiles, error } = await supabase
+            const { data, error } = await supabase
                 .from('perfiles')
-                .select('name');
+                .select('name')
+                .eq('id', id)
+                .single();
+            return data.name;
+
+            if (error) throw error;
+            return id;
+        } catch (error) {
+            console.error('Error loading partidas:', error);
+        }
+    }
+
+    async function loadEmail(id) {
+        try {
+            const { data, error } = await supabase
+                .from('users')
+                .select('email')
+                .eq('id', id)
+                .single();
 
             if (error) throw error;
 
-            return profiles;
+            return data.email; // Devuelve el email del usuario
         } catch (error) {
-            console.error('Error loading profiles:', error);
-            return [];
+            console.error('Error loading email:', error);
+            return null; // Retorna null en caso de error
         }
     }
 
@@ -59,19 +70,37 @@ export const VistaPartida = () => {
                 <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
                     <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                     <tr>
-                        <th scope="col" className="py-3 px-6">ID</th>
+                        <th scope="col" className="py-3 px-6">Id</th>
+                        <th scope="col" className="py-3 px-6">Nom</th>
                         <th scope="col" className="py-3 px-6">Usuario</th>
                         <th scope="col" className="py-3 px-6">Puntuación</th>
+                        <th scope="col" className="py-3 px-6">Clicks</th>
                         <th scope="col" className="py-3 px-6">Fecha</th>
                         <th scope="col" className="py-3 px-6">Hora</th>
                     </tr>
                     </thead>
                     <tbody>
+
                     {partidas.map((partida) => (
                         <tr key={partida.id} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                            <td className="py-4 px-6">{partida.id}</td>
+
+                            <td className="py-4 px-6">
+
+                                {
+                                    partida.id === user.id ? (
+                                        <button>Delete</button>
+                                    ) : (
+                                        <button>No-Delete</button>
+                                    )
+                                }
+
+                                {partida.id}
+
+                            </td>
                             <td className="py-4 px-6">{partida.usuari}</td>
+                            <td className="py-4 px-6">{user.email}</td>
                             <td className="py-4 px-6">{partida.puntuacion}</td>
+                            <td className="py-4 px-6">{totalClicks}</td>
                             <td className="py-4 px-6">{partida.data}</td>
                             <td className="py-4 px-6">{partida.hora}</td>
                         </tr>
@@ -80,5 +109,6 @@ export const VistaPartida = () => {
                 </table>
             </div>
         </div>
+
     );
 }
